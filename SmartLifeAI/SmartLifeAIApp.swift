4027b943afe10f95bd59b696b391712e24a197ad
Abhishek <abhishek12439525@gmail.com>
2026-02-13 05:20:16 +0530

    Main View

--- SmartLifeAI/SmartLifeAIApp.swift
+++ SmartLifeAI/SmartLifeAIApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct SmartLifeAIApp: App {
+
+    @StateObject private var appState = AppState() // ✅ CREATE ONCE
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(appState) // ✅ INJECT HERE
         }
     }
 }

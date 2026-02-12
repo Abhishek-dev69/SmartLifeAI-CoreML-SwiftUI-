0da6ebf6900ccf127f609dd735f40100f90bd7b6
Abhishek <abhishek12439525@gmail.com>
2026-02-12 10:31:07 +0530

    Initial Commit

---
+++ SmartLifeAI/SmartLifeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartLifeAIApp.swift
+//  SmartLifeAI
+//
+//  Created by Abhishek on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SmartLifeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartLifeAITests/SmartLifeAITests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartLifeAITests.swift
+//  SmartLifeAITests
+//
+//  Created by Abhishek on 12/02/26.
+//
+
+import Testing
+@testable import SmartLifeAI
+
+struct SmartLifeAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
